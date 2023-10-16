112f5532535935c888d8280dfd20dd6e11b62426
Augusto Jerônimo <dev.augustoj@gmail.com>
2023-10-16 13:33:54 -0300

    Update merge

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -1,320 +1,384 @@
-var fieldSize;              // Tamanho do campo (1x1)
-var matriz = [];            // Matriz onde a cobra é marcada
-var snake = [
-    {line: 0, column: 0},
-    {line: 0, column: 0},
-    {line: 0, column: 0}
-];
-var key;                    // Armazena a chave de tecla pressionada
-var directionValid = "w";   // Armazena a direção quando ela é válida
-var loop;                   // Armazena o loop de jogo
+/* - - - Variáveis para processamento interno - - - */
 
-var points = 0;
+var fieldSize;          // Tamanho do campo
+var matriz;             // Campo
+var snake;              // Cobra
 
-/* Captura dos evnetos de teclado */
-addEventListener("keydown", (e) => {
-    key = e.key;
-    validMove();
-})
+var points = 0;         // Pontos
+var pointsGoal;         // Meta de pontos
 
-function mobileControl(direction) {
-    key = direction;
-    validMove();
+var key;                // Valor de qualquer tecla pressionada
+var direction;          // Direção capturada por "key"
+
+var loop;               // Loop do jogo
+var finished;           // O jogo acabou?
+var win;                // Houve vitória
+
+
+/* - - - Elementos HTML - - - */
+
+var screen = document.querySelector('#screen');     // Campo onde o é exibido
+var timer = document.querySelector('#timer');       // Contagem regressiva
+var menu = document.querySelector('#menu');         // Menu de início
+var endGame = document.querySelector('#endGame');   // Tela final
+var message = document.querySelector('#message');   // Mensagem de vitória/derrota
+var score = document.querySelector('#score');       // Pontuação
+
+
+/* - - - Funções, onde a mágica acontece - - - */
+
+/* Captura do evento de teclado */
+addEventListener("keydown", (event) => {
+    key = event.key;
+
+    keyAction();
+});
+
+/* Decide o que fazer de acordo com a tecla pressionada */
+function keyAction() {
+    /* Caso seja uma tecla direcional (a, w, s, d ou setas) */
+    if ('awsd'.includes(key) || key.includes('Arrow')) {
+        /* Atribui a nova direção de acordo com a tecla e a direção anterior da cobra */
+        if ((key == 'w' || key == 'ArrowUp') && snake[0].line <= snake[1].line) {
+            direction = '^'; // cima
+        } else if ((key == 's' || key == 'ArrowDown') && snake[0].line >= snake[1].line) {
+            direction = 'v'; // baixo
+        } else if ((key == 'a' || key == 'ArrowLeft') && snake[0].column <= snake[1].column) {
+            direction = '<'; // esquerda
+        } else if ((key == 'd' || key == 'ArrowRight') && snake[0].column >= snake[1].column) {
+            direction = '>'; // direito
+        }
+    }
+
+    if (finished) {
+        
+        switch (key) {
+            case 'Escape':
+                manageScreen(menu, 'block');
+                break;
+            case 'Enter':
+                startGame();
+                break;
+        }
+
+    }
 }
 
-/* Inicia o loop de jogo */
-function start() {
-    /* a cada 300ms move a cobra de acordo com a tecla e atualiza a tela */
-    loop = setInterval(() => {
-        moveSnake();
-        updateScreen();
-    }, 300);
+/* Inicia o loop de jogo, que move a cobra e atualiza a tela */
+function play() {
+    /*Contagem regressiva*/
+    let seconds = 3;
+
+    let timerLoop = setInterval(() => {
+        /* Conta até 0, então para o timer e inicia o loop de jogo */
+        if (seconds >= 0) {
+            if (seconds > 0) {
+                timer.innerText = seconds;
+            } else {
+                timer.innerText = 'Go!';
+            }
+
+        } else {
+            timer.innerText = '';
+            clearInterval(timerLoop);
+
+            /* Loop de jogo que move a cobra e atualiza a tela até que o jogo acabe */
+            loop = setInterval(() => {
+                moveSnake();
+                updateScreen();
+        
+                if (finished) {
+                    manageScreen(endGame, 'block');
+                    pause();
+                }
+            }, 350);
+
+        }
+
+        seconds--;
+    }, 1000);
+
+    
 }
 
 /* Para o loop de jogo */
-function stop() {
+function pause() {
     clearInterval(loop);
-}
-
-/* Função que cria a tela no html e as matrizes correspondentes no JavaScript */
-function createScreen(size = 9) {
-    /* Coreção de tabuleiro em caso de vavlor inválido */
-    if (size % 2 == 0 || size < 9) {
-        size = 9;
-    }
-    /* Variável global de tamanho do campo recebe o valor recebido por parâmetro */
-    fieldSize = size;
-
-    /* Preenchimento das matrizes e criação da tela */
-    for (let l = 0; l < size; l++) {
-        let section = document.createElement("section");
-        section.setAttribute("class", "line");
-
-        let lineA = [];
-
-        for (let c = 0; c < size; c++) {
-            let div = document.createElement("div");
-            div.setAttribute("id", `div${l}-${c}`);
-            div.setAttribute("class", "spot");
-            section.appendChild(div);
-
-            lineA.push(0);
-        }
-
-        document.getElementById("screen").appendChild(section);
-
-        matriz.push(lineA);
-
-    }
-
-    let row = document.querySelectorAll(".line");
-    let column = document.querySelectorAll(".spot");
-
-    for (let i = 0; i < row.length; i++) {
-        row[i].style.height = `${100/size}%`;
-    }
-
-    for (let i = 0; i < column.length; i++) {
-        column[i].style.width = `${100/size}%`;
-    }
-    
-}
-
-/* Função que calcula a posição da cobra na matriz */
-function createSnake() {
-    let midOfField = parseInt(fieldSize/2); // coluna em que a cobra vai iniciar o jogo (meio do tabuleiro)
-
-    snake[0].line = midOfField-1;
-    snake[0].column = midOfField;
-
-    snake[1].line =midOfField;
-    snake[1].column = midOfField;
-
-    snake[2].line =midOfField+1;
-    snake[2].column = midOfField;
-
-    setSnakePosition();
     updateScreen();
 }
 
-/* Marca a cobra na matriz */
-function setSnakePosition() {
-    let n = 1;
-    
-    for (let l = 0; l < snake.length; l++) {
-        
-        matriz[snake[l].line][snake[l].column] = n;
-        n++;
-        
-    }
-
+/* Reseta as variáveis para início de jogo */
+function reset() {
+    finished = false;
+    win = false;
+    points = 0;
+    direction = '^';
 }
 
-/* Atualiza a tela conforme os dados armazenados */
-function updateScreen() {
+function manageScreen(scr, state) {
+    menu.style.display = 'none';
+    endGame.style.display = 'none';
 
-    for (let l = 0; l < fieldSize; l++) {
-        for (let c = 0; c < fieldSize; c++) {
-
-            /* Variável que contém a divi respectiva à posição da matriz */
-            let spot = document.querySelector(`#div${l}-${c}`);
-            /* Reseta a lista de classes, deixando marcado apenas como posição */
-            spot.setAttribute("class", "spot");
-
-            /* De acordo com o valor da matriz na posição, define a classe correspondente */
-            switch (matriz[l][c]) {
-                /* Fruta */
-                case -1:
-                    spot.classList.add("fruit")
-                    break;
-                /* Grama */
-                case 0:
-                    spot.classList.add("grass");
-                    break;
-                /* Cobra */
-                default:
-                    spot.classList.add("snake");
-                    break;
-            }
-
-        }
-    }
-}
-
-/* Impede que a direção seja contrária ao movimento */
-function validMove() {
-    /* Se a tecla apertada foi uma de controle, continua o teste */
-    if ("awsd".includes(key)) {
-        /* Para ir em uma direção, não pode estar vindo dela */
-        switch (key) {
-            case "w":
-                if (matriz[snake[0].line-1][snake[0].column] != 2) {
-                    directionValid = key;
-                }
-                break;
-            case "s":
-                if (matriz[snake[0].line+1][snake[0].column] != 2) {
-                    directionValid = key;
-                }
-                break;
-            case "a":
-                if (matriz[snake[0].line][snake[0].column-1] != 2) {
-                    directionValid = key;
-                }
-                break;
-            case "d":
-                if (matriz[snake[0].line][snake[0].column+1] != 2) {
-                    directionValid = key;
-                }
-                break;
-        }
-    }
-
-}
-
-/* Realiza todos os testes necessários e move a cobra na matriz caso seja possível */
-function moveSnake() {
-    /* Todas as possibilidades de bater em uma parede */
-    let cond1 = directionValid == "w" && snake[0].line == 0;
-    let cond2 = directionValid == "s" && snake[0].line == fieldSize-1;
-    let cond3 = directionValid == "a" && snake[0].column == 0;
-    let cond4 = directionValid == "d" && snake[0].column == fieldSize-1;
-
-    /* Caso tenha batido em uma parede, para o jogo e encerra a função */
-    if (cond1 || cond2 || cond3 || cond4) {
-        stop();
-        console.log("parede!");
+    if (scr == null) {
         return;
     }
 
-    /* Todas as possibilidades de bater em uma parede */
-    cond1 = directionValid == "w" && matriz[snake[0].line-1][snake[0].column] > 0;
-    cond2 = directionValid == "s" && matriz[snake[0].line+1][snake[0].column] > 0;
-    cond3 = directionValid == "a" && matriz[snake[0].line][snake[0].column-1] > 0;
-    cond4 = directionValid == "d" && matriz[snake[0].line][snake[0].column+1] > 0;
+    scr.style.display = state;
 
-    /* Caso o a cobra avance no próprio corpo, para o jogo e encerra a função */
-    if (cond1 || cond2 || cond3 || cond4) {
-        stop();
-        console.log("corpo!");
-        return;
-    }
+    score.innerText = points;
 
-    /* Todas as possibilidades de comer uma fruta */
-    cond1 = directionValid == "w" && matriz[snake[0].line-1][snake[0].column] == -1;
-    cond2 = directionValid == "s" && matriz[snake[0].line+1][snake[0].column] == -1;
-    cond3 = directionValid == "a" && matriz[snake[0].line][snake[0].column-1] == -1;
-    cond4 = directionValid == "d" && matriz[snake[0].line][snake[0].column+1] == -1;
-
-    /* Se comeu uma fruta, aumenta uma seção na cobra */
-    if (cond1 || cond2 || cond3 || cond4) {
-        snake.push({line: 0, column: 0});
-        createFruit();
-        console.log("fruta!");
-    }
-
-    
-    /* Move o corpo, cada seção tomando o lugar próxima */
-    for (let s = snake.length-1; s > 0; s--) {
-        snake[s].line = snake[s-1].line;
-        snake[s].column = snake[s-1].column;
-    }
-    /* Move a cabeça da cobra */
-    switch (directionValid) {
-        case "w":
-            snake[0].line--;
-            break;
-        case "s":
-            snake[0].line++;
-            break;
-        case "a":
-            snake[0].column--;
-            break;
-        case "d":
-            snake[0].column++;
-            break;
-    }
-
-    updateMove();
-}
-
-/* Limpa o campo e remarca a cobra na posição atual */
-function updateMove() {
-    
-    for (let l = 0; l < fieldSize; l++) {
-        for (let c = 0; c < fieldSize; c++) {
-            
-            if (matriz[l][c] != -1) {
-                matriz[l][c] = 0;
-            }
-            
-        }
-    }
-
-    for (let s = 0; s < snake.length; s++) {
-        matriz[snake[s].line][snake[s].column] = s+1;        
-    }
-
-}
-
-/* Função que cria a fruta em um ponto livre do campo */
-function createFruit() {
-    while (true) {
-        
-        let x = parseInt(Math.random() * fieldSize-1);
-        let y = parseInt(Math.random() * fieldSize-1);
-
-        if (matriz[y][x] == 0) {
-            matriz[y][x] = -1;
-            return;
-        }
-
+    if (win) {
+        message.innerText = 'Victory';
+    } else {
+        message.innerText = 'Game over'
     }
 }
 
-/* Sequencia seguida para início da partida e loop de jogo */
-function game(size) {
-    createScreen(size);
+/* Configura o tamanho do campo e a meta de pontos, chama a função que inicia o jogo */
+function configureAndStart(size, goal) {
+    fieldSize = size;
+    pointsGoal = goal;
+    startGame();
+}
+
+/* Inicia o jogo */
+function startGame() {
+    /* Menu desaparecem */
+    manageScreen(null);
+
+    /* Reseta o jogo e cria o ambiente */
+    reset();
+    createScreen();
     createSnake();
     createFruit();
     updateScreen();
-    
-    setTimeout(() => {
-        start();
-    }, 1500);
 
+    play();
 }
-/* Função que starta o jogo */
-function play() {
-    /* Cria o campo de acordo com o tamanho escolhido */
-    let opt = document.querySelector('input[name="opt"]:checked').id;
 
-    document.querySelector("#b1").classList.add("hidden");
+/* Cria o campo, em matriz e em HTML  */
+function createScreen() {
+    /* Limpa qualquer conteúdo anterior da matriz */
+    matriz = [];
 
-    switch (opt) {
-        case "opt1":
-            game(9);
+    /* preenche a matriz com 0 */
+    for (let i = 0; i < fieldSize; i++) {
+        let row = []; // Cria uma linha
+        
+        for (let c = 0; c < fieldSize; c++) {
+            row.push(0); // Preenche a linha com 0
+        }
+
+        matriz.push(row); // Adiciona a linha na matriz
+    }
+
+    /* Limpa qualquer conteúdo anterior do campo no HTML */
+    screen.innerHTML = '';
+
+    /* cria linhas e adiciona pontos nelas, depois as adiciona no campo */
+    for (let l = 0; l < fieldSize; l++) {
+
+        let row = document.createElement('section');    // Linha criada
+        row.style.height = `${100/fieldSize}%`;         // altura da linha definida
+        
+        for (let c = 0; c < fieldSize; c++) {
+
+            let spot = document.createElement('div');   // Ponto criado
+            spot.setAttribute('class', 'grass');        // Ponto é marcado como grama
+            spot.setAttribute('id', `spot${l}-${c}`);   // Ponto ganha um id
+            spot.style.width = `${100/fieldSize}%`;     // Largura do ponto definida
+            row.appendChild(spot);                      // Ponto adicionado a linha
+
+        }
+
+        screen.appendChild(row);                        // Linha adicionada ao campo
+        
+    }
+}
+
+/* Cria a cobra no centro do campo */
+function createSnake() {
+
+    /* Cento do campo (vertical e horizontal) */
+    let center = parseInt(fieldSize/2); 
+
+    /* Cria a cobra */
+    snake = [
+        {line: center-1,column: center},
+        {line: center,column: center},
+        {line: center+1,column: center}
+    ];
+
+    markSnake();
+}
+
+/* Marca a cobra no campo */
+function markSnake() {
+    /* Limpa a posição anterior da cobra */
+    for (let l = 0; l < fieldSize; l++) {
+        for (let c = 0; c < fieldSize; c++) {
+            
+            if (matriz[l][c] > 0) {
+                matriz[l][c] = 0;
+            }
+
+        }
+    }
+
+    /* Maraca na matriz os números correspondetes a cada seção da cobra */
+    for (let s = 0; s < snake.length; s++) {
+        matriz[snake[s].line][snake[s].column] = s+1;
+    }
+}
+
+/* Move a cobra de acordo com a direção escolhida */
+function moveSnake() {
+    /* Se houve colisão, não move a cobra */
+    if (lose()) {
+        pause();
+        return;
+    }
+
+    /* Move cada seção da cobra para a proxima posição, a partir do fim */
+    for (let s = snake.length-1; s > 0; s--) {
+        
+        snake[s].line = snake[s-1].line;
+        snake[s].column = snake[s-1].column;
+        
+    }
+
+    /* Move a cabeça da cobra na direção escolhida */
+    switch (direction) {
+        case '^':
+            snake[0].line--;
             break;
-        case "opt2":
-            game(15);
+        case 'v':
+            snake[0].line++;
             break;
-        case "opt3":
-            game(21);
+        case '<':
+            snake[0].column--;
+            break;
+        case '>':
+            snake[0].column++;
             break;
         default:
-            console.error("Erro...");
+            console.log('Algo de errado não está certo...');
             break;
+    }
+
+    /* Marca a alteração na matriz */
+    markSnake();
+}
+
+/*  Checa a colisão da cobra com o corpo e as paredes, contabiliza os pontos.
+    Retorna true se o jogo acabou, seja por derrota ou vitória */
+function lose() {
+    /* Possibilidades de bater em uma parede */
+    if (direction == '^' && snake[0].line == 0) {
+        /* Bateu no teto */
+        finished = true;
+        return true;
+    } else if (direction == 'v' && snake[0].line == fieldSize-1) {
+        /* Bateu no chão */
+        finished = true;
+        return true;
+    } else if (direction == '<' && snake[0].column == 0) {
+        /* Bateu na parede da esquerda */
+        finished = true;
+        return true;
+    } else if (direction == '>' && snake[0].column == fieldSize-1) {
+        /* Bateu na parede da direita */
+        finished = true;
+        return true;
+    }
+
+    /* proxima posição */
+    let nextPosition;
+
+    switch (direction) {
+        case '^':
+            nextPosition = matriz[snake[0].line-1][snake[0].column]
+            break;
+        case 'v':
+            nextPosition = matriz[snake[0].line+1][snake[0].column]
+            break;
+        case '<':
+            nextPosition = matriz[snake[0].line][snake[0].column-1]
+            break;
+        case '>':
+            nextPosition = matriz[snake[0].line][snake[0].column+1]
+            break;
+        default:
+            console.log('Algo de errado definitivamente não está certo...');
+            break;
+    }
+
+    /* Proxima posição é ocupada pelo corpo da cobra */
+    if (nextPosition > 0) {
+        finished = true;
+        return true;
+    }
+
+
+    /* Proxima posição é ocupada por uma fruta */
+    if (nextPosition == -1) {
+        /* Adiciona um ponto, uma seção na cobra e cria uma nova fruta */
+        points++;
+        snake.push({line: 0, column:0});
+        createFruit();
+    }
+
+    /* Se atingiu a meta de pontos, declara vitória e jogo finalizado */
+    if (points == pointsGoal) {
+        win = true;
+        finished = true;
+    }
+
+    return false;
+
+}
+
+/* Cria uma fruta em uma posição aleatória do campo */
+function createFruit() {
+    while (true) {
+        /* Valores aleatórios para linha e coluna */
+        let l = parseInt(Math.random() * fieldSize);
+        let c = parseInt(Math.random() * fieldSize);
+
+        /* Se a posição estiver vazia, coloca uma fruta e encerra a repetição */
+        if (matriz[l][c] == 0) {
+            matriz[l][c] = -1;
+            return;
+        }
+        
     }
 }
 
-/* Menu */
+/* Atualiza a tela */
+function updateScreen() {
+    
+    for (let l = 0; l < fieldSize; l++) {
+        for (let c = 0; c < fieldSize; c++) {
+            /* Captura o elemento correspondente à posição na matriz */
+            let spot = document.querySelector(`#spot${l}-${c}`);
 
-function changeOption(btn) {
-    let btns = document.querySelectorAll(".size-option");
-
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].classList.remove("marked");       
+            /* Percorre a matriz adicionando as classes aos respectivos elementos HTML */
+            switch (matriz[l][c]) {
+                case -1:
+                    spot.setAttribute('class', 'fruit');
+                    break;
+                case 0:
+                    spot.setAttribute('class', 'grass');
+                    break;
+                case 1:
+                    spot.setAttribute('class', 'snakeHead');
+                    break;
+                default:
+                    spot.setAttribute('class', 'snakeBody');
+                    break;
+            }
+            
+        }
     }
 
-    btn.classList.add("marked");
 }
